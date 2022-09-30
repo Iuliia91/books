8215371e0d11b3b51e2c67c494cca38947645fc9
Iuliia91 <iuliiaprusakova91@gmail.com>
2022-09-30 16:10:04 +0300

    working

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import BookCard from '../components/BookCard'
 import styles from '../styles/Home.module.scss'
 
@@ -15,13 +16,48 @@ export const getStaticProps = async () => {
   }
 }
 
+const dinamicPagination = async (number) => {
+  const response = await fetch(`https://gutendex.com/books/?page=${number}`)
+  const data = await response.json()
+
+  return data
+}
+
 const Home = ({ booksList }) => {
-  const { results, url } = booksList
+  const { results, next } = booksList
+  const [currentPage, setCurrentPage] = useState(1)
+  const [list, setList] = useState(results)
+  const [fetching, setFetching] = useState(false)
+
+  useEffect(() => {
+    dinamicPagination(currentPage).then((response) => {
+      setCurrentPage((prev) => prev + 1)
+      setList([...list, ...response.results])
+    })
+  }, [fetching])
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+
+    return function () {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) ===
+      0
+    ) {
+      setFetching(true)
+    }
+  }
 
   return (
     <div className={styles.container} onClick={() => handleAdd(url)}>
       {booksList &&
-        results.map((item) => {
+        list.map((item) => {
           return <BookCard item={item} />
         })}
     </div>
