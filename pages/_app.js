fe4f087063f5f3fc59fc24c00d260ec9dbae6d30
Iuliia91 <iuliiaprusakova91@gmail.com>
2022-10-11 22:30:09 +0300

    vf

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 import Header from '../components/Header'
 import AppWraper from '../context/AppContext'
 
@@ -6,7 +7,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <meta name="description" content="search and read books" />
+        <title>Book search</title>
+        <meta
+          name="description"
+          content="search and read books. You can find everithyng"
+        />
       </Head>
       <AppWraper>
         <Header />
