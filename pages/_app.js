dab6a65d6d8de2ce89239a27b318504ef25f9b95
Iuliia91 <iuliiaprusakova91@gmail.com>
2022-10-11 22:33:12 +0300

    bv

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,16 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import AppWraper from '../context/AppContext'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Head>
+/*<Head>
         <title>Book search</title>
         <meta
           name="description"
           content="search and read books. You can find everithyng"
         />
-      </Head>
+      </Head>*/
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
       <AppWraper>
         <Header />
         <Component {...pageProps} />
